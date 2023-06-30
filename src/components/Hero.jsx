fb048a9e4d3a1ee5d70ce9af5ea295c082c00e6e
Cyrus Roy <cyrus.roy001@gmail.com>
2023-06-30 21:13:07 +0530

    first

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+        <div className="mt-7">
+          <h1 className="head_text">
+            Summarize Articles with <br className="max-md:hidden" />
+            <span className="orange_gradient">Articlio GPT-4</span>
+          </h1>
+
+          <h2 className="desc">
+            Simplify your reading with <strong>Articlio</strong>, an open-source article summarizer that transforms lengthy articles into a clear and concise form
+          </h2>
+        </div>
+    </header>
+  )
+}
+
+export default Hero
